92ce5296b5f867bcdc174ac60c98c025d860cc6f
Yasmine <benltaiefyassmine@gmail.com>
2026-01-08 10:43:08 +0100

    1er commit

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,14 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	public void Gaulois(String nom,int force) {
+		this.nom=nom;
+		this.force=force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+}
